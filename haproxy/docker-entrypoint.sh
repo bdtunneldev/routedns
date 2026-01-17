2e689c318e8b54c494e24667edf857b93805b97d
bdtunneldev <info@bdtunnel.com>
2026-01-17 09:25:53 +0400

    Refactor environment variable substitution in entrypoint
    Replace envsubst command with a new line for clarity.

--- haproxy/docker-entrypoint.sh
+++ haproxy/docker-entrypoint.sh
@@ -5,7 +5,7 @@ CONFIG_TEMPLATE="/usr/local/etc/haproxy/haproxy.cfg"
 CONFIG_FILE="/tmp/haproxy.cfg"
 
 # Substitute environment variables in config
-envsubst '${HAPROXY_STATS_USER} ${HAPROXY_STATS_PASSWORD}' < "$CONFIG_TEMPLATE" > "$CONFIG_FILE"
+
 
 # Start HAProxy with the processed config
 exec haproxy -f "$CONFIG_FILE" "$@"
